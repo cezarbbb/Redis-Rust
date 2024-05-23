c8fab4b8e9ba6671f519acb53d2bbac2df2468d8
cezarbbb <btq19991016@gmail.com>
2024-05-23 23:57:38 +0900

    pass stage

--- src/port.rs
+++ src/port.rs
@@ -1,4 +1,4 @@
-use tokio::{net::TcpStream, io::AsyncWriteExt};
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpStream};
 use crate::resp::Value;
 
 #[derive(Clone, Copy, Debug)]
@@ -45,10 +45,16 @@ pub async fn send_hand_shake(host: String, master_port_id: String, cur_port_id: 
 
     let hs_1 = Value::Array(vec![Value::BulkString("PING".to_string())]);
     hand_shake.write(hs_1.serialize().as_bytes()).await.expect("Handshake 1 failed");
+    let mut ping_response_buffer = [0; 1024];
+    hand_shake.read(&mut ping_response_buffer).await.expect("Failed to receive ping response when handshaking");
 
     let hs_2 = Value::Array(vec![Value::BulkString("REPLCONF".to_string()), Value::BulkString("listening-port".to_string()), Value::BulkString(format!("{}", cur_port_id).to_string())]);
     hand_shake.write(hs_2.serialize().as_bytes()).await.expect("Handshake 1/2 failed");
+    let mut replconf_listening_port_response = [0; 1024];
+    hand_shake.read(&mut replconf_listening_port_response).await.expect("Failed to receive response ");
 
     let hs_3 = Value::Array(vec![Value::BulkString("REPLCONF".to_string()), Value::BulkString("capa".to_string()), Value::BulkString("psync2".to_string())]);
     hand_shake.write(hs_3.serialize().as_bytes()).await.expect("Handshake 2/2 failed");
+    let mut replconf_capa_response = [0; 1024];
+    hand_shake.read(&mut replconf_capa_response).await.expect("Failed to receive capa responose when handshaking");
 }
