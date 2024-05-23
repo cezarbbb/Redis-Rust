dcd717644ef5f6813889f1e72538d8fb31ad30aa
cezarbbb <btq19991016@gmail.com>
2024-05-23 18:15:17 +0900

    pass stage

--- src/info.rs
+++ src/info.rs
@@ -1,12 +1,8 @@
 use crate::resp::Value;
 
 pub fn get_info(is_master: bool) -> Value {
-    let mut info = vec![];
-    let role = Value::BulkString(format!("role:{}", if is_master {"master"} else {"slave"}).to_string());
-    let master_replid = Value::BulkString("master_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb".to_string());
-    let master_repl_offset = Value::BulkString("master_repl_offset:0".to_string());
-    info.push(role);
-    info.push(master_replid);
-    info.push(master_repl_offset);
-    Value::Array(info)
+    let role = format!("role:{}\r\n", if is_master {"master"} else {"slave"});
+    let master_replid = "master_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb\r\n";
+    let master_repl_offset = "master_repl_offset:0\r\n";
+    Value::BulkString(role + master_replid + master_repl_offset)
 }
