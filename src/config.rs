855aa8008004c52b8e542a3f0c09e11d974bd9cc
cezarbbb <btq19991016@gmail.com>
2024-05-25 00:50:06 +0900

    pass stage

--- src/config.rs
+++ src/config.rs
@@ -36,16 +36,41 @@ impl Config {
     pub fn parse() -> Self {
         let args = env::args().collect::<Vec<String>>();
         let mut config = Config::new();
-        
         config.port = match args.iter().position(|arg| arg == "--port") {
             Some(index) => args.get(index + 1).unwrap().to_string(),
             None => "6379".to_string(),
         };
         config.replicaof = match args.iter().position(|arg| arg == "--replicaof") {
-            Some(index) => Some(args.get(index + 1).unwrap().to_string()),
+            Some(index) => {
+                let mport_params = args.get(index + 1).unwrap().split(' ').collect::<Vec<&str>>();
+                Some(format!("{}:{}", mport_params[0], mport_params[1]))
+            },
             None => None,
         };
-        println!("***********{}{:?}", &config.port, &config.replicaof);
+
+        // let cur_port_id = match args.iter().position(|arg| arg == "--port") {
+        //     Some(index) => args.get(index + 1).unwrap(),
+        //     None => "6379",
+        // };
+        // let (cur_port, master_port);
+        // let master_port = match args.iter().position(|arg| arg == "--replicaof") {
+        //     Some(index) => {
+        //         let mport_params = args.get(index + 1).unwrap().split(' ').collect::<Vec<&str>>();
+        //         let (host, mport) = (mport_params[0], mport_params[1]);
+
+        //         master_port = Port::new(mport.to_string(), PortType::Master);
+        //         cur_port = Port::new(cur_port_id.to_string().clone(), PortType::Slave);
+
+        //         handshake::send_hand_shake(host.to_string(), master_port.id.clone(), cur_port.id.clone()).await;
+
+        //         master_port
+        //     },
+        //     None => {
+        //         master_port = Port::new(cur_port_id.to_string().clone(), PortType::Master);
+        //         cur_port = Port::new(cur_port_id.to_string().clone(), PortType::Master);
+        //         master_port
+        //     },
+        // };
         // for (index, arg) in args.iter().enumerate() {
         //     if arg == "--port" {
         //         if let Some(port) = args.get(index + 1) {
