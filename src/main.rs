f74ad92abfc8c67358085bd3ac0c090528e165da
cezarbbb <btq19991016@gmail.com>
2024-05-25 01:07:15 +0900

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -101,7 +101,7 @@ async fn handle_conn(stream: TcpStream, redis_server: Arc<RedisServer>, sender: 
             }
         }
 
-        sender.send(command_propagate.clone());
+        let _ = sender.send(command_propagate.clone());
     }
 }
 
