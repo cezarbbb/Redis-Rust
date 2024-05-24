6779af7cfe1f5a788b43c8e7dd0cc2d1519f076f
cezarbbb <btq19991016@gmail.com>
2024-05-24 15:50:27 +0900

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -104,7 +104,7 @@ async fn handle_conn(stream: TcpStream, is_master: bool) {
         handler.write_value(response).await.unwrap();
 
         if if_send_rdb {
-            handler.write_value(Value::RDBString("111".to_string())).await.unwrap();
+            handler.write_value(Value::RDBString("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2".to_string())).await.unwrap();
         }
     }
 }
