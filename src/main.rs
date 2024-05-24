8f6524cd45af0287539e592fec14425286b96736
cezarbbb <btq19991016@gmail.com>
2024-05-24 23:57:42 +0900

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -94,6 +94,7 @@ async fn handle_conn(stream: TcpStream, redis_server: Arc<RedisServer>, sender: 
         if if_subscribe {
             let mut receiver = sender.subscribe();
             while let Ok(f) = receiver.recv().await {
+                println!("receiver receive {:?}", f);
                 handler.write_value(f).await.unwrap();
             }
         }
